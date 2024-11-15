25c76102c17efdf90899459f47eef9205491641c
Andrey <dr.af2015@yandex.ru>
2024-11-15 16:35:53 +0300

    Выполнено

--- modul 10/02 task/main.py
+++ modul 10/02 task/main.py
@@ -27,4 +27,5 @@ class Knight(threading.Thread):
 first_knight = Knight('Sir Lancelot', 10)
 second_knight = Knight("Sir Galahad", 20)
 first_knight.start()
+first_knight.join()
 second_knight.start()
